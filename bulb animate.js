aed0ce0df530b034efadc48e54f469c2d9fbe33d
Vijay8452 <167493895+Vijay8452@users.noreply.github.com>
2024-08-02 15:30:04 +0530

    Add files via upload

---
+++ bulb animate.js
@@ -0,0 +1,7 @@
+document.getElementById('turnOnButton').onclick = function() {
+    document.getElementById('myImage').src = 'bulbon.gif';
+};
+
+document.getElementById('turnOffButton').onclick = function() {
+    document.getElementById('myImage').src = 'bulboff.gif';
+};
